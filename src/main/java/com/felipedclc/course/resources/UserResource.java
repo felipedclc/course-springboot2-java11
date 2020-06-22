49a1ae605e85d53917514ca574b2532c3210c2dc
Felipe Cardoso <felipedclc@gmail.com>
2020-06-22 17:52:08 -0300

    User entity and resource

---
+++ src/main/java/com/felipedclc/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.felipedclc.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.felipedclc.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource { 
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){ //RETORNA RESPOSTA DE REQUISIÇÕES WEB
+		User u = new User(1, "Maria do Carmo", "mc@gmail.com", "99999999", "12345");
+		return ResponseEntity.ok().body(u); // CORRESPONDE AO METODO GET
+	}
+}

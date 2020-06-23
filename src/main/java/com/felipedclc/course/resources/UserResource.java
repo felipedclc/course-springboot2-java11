883bb38f918cf4ed44fb682069eb71e8717f3d60
Felipe Cardoso <felipedclc@gmail.com>
2020-06-23 11:13:56 -0300

    Service layer, component registration

--- src/main/java/com/felipedclc/course/resources/UserResource.java
+++ src/main/java/com/felipedclc/course/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.felipedclc.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felipedclc.course.entities.User;
+import com.felipedclc.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource { 
 	
-	@GetMapping
-	public ResponseEntity<User> findAll(){ //RETORNA RESPOSTA DE REQUISIÇÕES WEB
-		User u = new User((long) 1, "Maria do Carmo", "mc@gmail.com", "99999999", "12345");
-		return ResponseEntity.ok().body(u); // CORRESPONDE AO METODO GET
+	@Autowired // INJEÇÃO AUT
+	private UserService service;
+	 
+	@GetMapping // REQUISIÇÕES DO TIPO GET
+	public ResponseEntity<List<User>> findAll(){ //RETORNA RESPOSTA DE REQUISIÇÕES WEB
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list); // CORRESPONDE AO METODO GET
+	}
+	
+	@GetMapping(value = "/{id}") // REQUISIÇÃO ACEITA O "ID" DENTRO DA URL
+	public ResponseEntity<User> findById(@PathVariable Long id){ // "ANOTATION" PARA O SPRING CONSIDERAR O ID COMO PARAMAETRO
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj); // END POINT 
 	}
 }
---
+++ src/main/java/com/felipedclc/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.felipedclc.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipedclc.course.entities.User;
+import com.felipedclc.course.repositories.UserRepository;
+
+@Service 
+public class UserService {
+
+	@Autowired // INJEÇÃO DE DEPENDÊNCIA AUTOMÁTICA
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get(); // RETORNA O OBJETO DENTRO DO "< >"
+	}
+}
